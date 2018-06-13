737b26e28b712dc7c766cbbe2b0605f9ecea9980
Yapkan Choi <y.k.choi@student.tudelft.nl>
2018-06-13 09:07:33 +0200

    changed colors of ROC curves

--- plots.py
+++ plots.py
@@ -123,19 +123,11 @@ def plot_ROC(figures_dir, filename, test_gen, test_length, label_encoder, predic
     plt.figure()
     plt.plot(fpr["micro"], tpr["micro"],
              label='micro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc["micro"]),
-             color='deeppink', linestyle='-', linewidth=4)
+                   ''.format(roc_auc["micro"]))
 
     plt.plot(fpr["macro"], tpr["macro"],
              label='macro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc["macro"]),
-             color='navy', linestyle='-', linewidth=4)
-
-    # colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
-    # for i, color in zip(range(num_classes), colors):
-    #     plt.plot(fpr[i], tpr[i], color=color, lw=lw,
-    #              label='ROC curve of class {0} (area = {1:0.2f})'
-    #                    ''.format(i, roc_auc[i]))
+                   ''.format(roc_auc["macro"]))
 
     plt.plot([0, 1], [0, 1], 'k--', lw=2)
     plt.xlim([0.0, 1.0])
